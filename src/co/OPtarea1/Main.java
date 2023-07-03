1f98a953158e6041d265d96b624b7e57cc9c1ca3
Andredesarrollo <simpandres24@gmail.com>
2023-07-02 23:03:45 -0500

    first commit

---
+++ src/co/OPtarea1/Main.java
@@ -0,0 +1,16 @@
+package co.OPtarea1;
+
+public class Main {
+    public static void main (String[] args){
+        long numeroTelefono = 1231153;
+        int numero = 11321;
+        short nume2 = 10;
+        Double num3 = 12.4d;
+        float num4 = 2.5f;
+        String Camisa = "Camisa";
+         boolean genero = true;
+
+         System.out.println(" junita  viven en " + numero + "su telefono es : " + numeroTelefono + "su edad " + nume2 + "su ropa favoirta es : " + Camisa + " su altura es :" + num4 + "ft");
+
+    }
+}
